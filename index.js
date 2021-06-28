16863e65a10ed40a359e2f987d4096d4d0f960a0
Aidan Perret <aperret@me.com>
2021-06-28 12:57:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
 
+let random = (Math.floor(Math.random() * 10) + 1)
+
+const num3 = 10
+const num4 = 6
+let mod = num3 % num4
+
+let max = Math.max(1, 2, 3, 20)
 // Write your code here
